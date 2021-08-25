ca9aa762433bd5f471c73a373fb307839506461f
YuliyaZakharchuk <yulua03071992@gmail.com>
2021-08-26 00:06:30 +0300

    change styles

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-let modal = document.querySelector(".modal");
-let show = document.querySelector(".show");
-let modalBox = document.querySelector(".modal-box")
-let closeButton = document.querySelector(".close-button");
+/* Modal window */
 
-function enterImage() {
-    modalBox.append(window.addEventListener('click', (e) => console.log(e.target)));
-}
+const show = document.querySelector(".show");
+const modal = document.querySelector(".modal");
+const modalBodyImage = document.querySelector(".modal-body-image");
+const closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
-    enterImage();
+function toggleModal(e) {
     modal.classList.toggle("show-modal");
-
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+    if (e.target === closeButton) {
+        return;
     }
+    modalBodyImage.setAttribute('src', e.target.src);
 }
 
 show.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 
+/* Theme */
 
+const body = document.querySelector("body");
+const setTheme = theme => body.className = theme;
 
-const setTheme = theme => document.documentElement.className = theme;
-
-document.getElementById('#theme-select').addEventListener('change', function() {
+document.getElementById('theme-select').addEventListener('change', function() {
     setTheme(this.value);
 });
-
-
-function myFunction() {
-    var element = document.getElementById("dark");
-    element.classList.toggle("dark-theme");
-}
